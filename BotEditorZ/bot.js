355f7571557d436da647d992a78ca449ab309a16
PedroCarrasco82 <contatopedrocarrasco@gmail.com>
2020-09-04 03:46:52 -0300

    refactoting config

--- BotEditorZ/bot.js
+++ BotEditorZ/bot.js
@@ -1,7 +1,7 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
-const config = require('./config.json');
+const config = require('./config/config.json');
 const botController = require('./controller/botController');
 
 function updatedState(){
